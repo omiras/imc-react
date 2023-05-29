226a09a82d4419b6231c3e530e9db8aa3a30eaee
Oscar Miras <oscar.miras@gmail.com>
2023-05-29 14:44:40 +0200

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [height, setHeight] = useState('');
+  const [weight, setWeight] = useState('');
+  const [showMessage, setShowMessage] = useState(false);
+
+  const handleClick = () => {
+    setShowMessage(true);
+  }
+
+  const result = weight / (height / 100) ** 2;
 
   return (
     <div class="container">
       <h1>BMI Calculator</h1>
-      <form>
-        <p><label>Height in CM: </label><input type="text" id="height" /></p>
-        <p><label>Weight in KG: </label><input type="text" id="weight" /></p>
-        <button>Calculate</button>
-        <div id="results">25.44</div>
-        <div id="weight-guide">
-          <h3>BMI Weight Guide</h3>
-          <p>Under Weight = Less than 18.6</p>
-          <p>Normal Range = 18.6 and 24.9</p>
-          <p>Overweight = Greater than 24.9</p>
-        </div>
-      </form>
+      <p><label>Height in CM: </label><input disabled={showMessage} type="text" id="height" value={height} onChange={(e) => setHeight(e.target.value)} /></p>
+      <p><label>Weight in KG: </label><input disabled={showMessage} type="text" id="weight" value={weight} onChange={(e) => setWeight(e.target.value)} /></p>
+      <button disabled={showMessage} onClick={handleClick}>Calculate</button>
+      {showMessage && <div id="results">{result.toFixed(2)}</div>}
+      <div id="weight-guide">
+        <h3>BMI Weight Guide</h3>
+        <p>Under Weight = Less than 18.6</p>
+        <p>Normal Range = 18.6 and 24.9</p>
+        <p>Overweight = Greater than 24.9</p>
+      </div>
     </div>
   )
 }
